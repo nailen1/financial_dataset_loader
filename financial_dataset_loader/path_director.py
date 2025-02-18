54358cf79f93b41bd5c61da3d8d0ea617d3a1717
JuneYoung <nietsnie1@gmail.com>
2025-02-19 01:57:46 +0900

    refactor: reorganize loader functions with _s3 and _local suffixes

--- financial_dataset_loader/path_director.py
+++ financial_dataset_loader/path_director.py
@@ -1,15 +1,18 @@
+import os
+
+BASE_DIR = 'data'
+
 FILE_FOLDER = {
-    'market': 'dataset-market',
-    '2160': 'dataset-menu2160',
-    '2160-snapshot': 'dataset-menu2160-snapshot',
-    '2205': 'dataset-menu2205',
-    '2205-snapshot': 'dataset-menu2205-snapshot',
-    '4165': 'dataset-menu4165',
-    # '8186-snapshot': 'dataset-menu8186-snapshot',
-    '8186-snapshot': 'dataset-fund',
-    'index': 'dataset-index',
-    'currency': 'dataset-currency',
-    'info': 'dataset-fund',
+    'market': os.path.join(BASE_DIR, 'dataset-market'),
+    '2160': os.path.join(BASE_DIR, 'dataset-menu2160'),
+    '2160-snapshot': os.path.join(BASE_DIR, 'dataset-menu2160-snapshot'),
+    '2205': os.path.join(BASE_DIR, 'dataset-menu2205'),
+    '2205-snapshot': os.path.join(BASE_DIR, 'dataset-menu2205-snapshot'),
+    '4165': os.path.join(BASE_DIR, 'dataset-menu4165'),
+    '8186-snapshot': os.path.join(BASE_DIR, 'dataset-menu8186-snapshot'),
+    'fund': os.path.join(BASE_DIR, 'dataset-fund'),
+    'index': os.path.join(BASE_DIR, 'dataset-index'),
+    'currency': os.path.join(BASE_DIR, 'dataset-currency'),
 }
 
 BUCKET_SYSTEM = 'dataset-system'
